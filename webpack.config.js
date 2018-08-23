2b5f4a9aa8e7fcfa707a4dec4a68d283f2400f4d
Nitish <nitishkr88@gmail.com>
2018-08-23 19:42:24 +0530

    project skeleton

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  output: {
+    library: 'PromiseRetryable',
+    libraryTarget: 'umd'
+  }
+}
